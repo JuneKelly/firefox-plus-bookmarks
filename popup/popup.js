52d22940f330973ccbd4addc963776d95f61833a
halbryard <35330227+halbryard@users.noreply.github.com>
2018-01-16 12:45:04 +0100

    better UI elements encapsulation
    HTML elements references are encapsulated in panels object and nested elements are encapsulated in a (new) specific panel object.
    Co-benefits:
    - each element reference initialization is run once via function closures;
    - fix: togglePanel function semantics fixed removing errorPanel special case.

--- popup/popup.js
+++ popup/popup.js
@@ -7,56 +7,76 @@
 
   var panels = function () {
 
-    var errorPanel = document.getElementById('error-panel');
-    var errorMessage = document.getElementById('error-message');
+    var errorPanel = function () {
+      var _panel = document.getElementById('error-panel');
+      var errorMessage = document.getElementById('error-message');
+      return {
+        show: function (message) {
+          errorMessage.innerText = message || 'Something went wrong :(';
+          togglePanel(_panel);
+        }
+      }
+    }();
+
     /* 
       UNUSED panel object loadingPanel:
       removed but still used as default starting panel
       (see activePanel initialization);
     // var loadingPanel = document.getElementById('loading-panel');
     */
-    var previewPanel = document.getElementById('preview-panel');
-    var setupHint = document.getElementById('setup-hint');
-    var settingsLink = document.getElementById('open-settings');
+
+    var previewPanel = function () {
+      var _panel = document.getElementById('preview-panel');
+      var titleElement = document.getElementById('title');
+      var urlElement = document.getElementById('url');
+      var mailLink = document.getElementById('mail-link');
+
+      return {
+        show: function (targetAddress, title, url) {
+          titleElement.innerText = title;
+          urlElement.innerText = url;
+
+          var encodedTitle = encodeURI(title);
+          var encodedUrl = encodeURI(url);
+          mailLink.href = (
+            'mailto:' + targetAddress + '?subject=' + encodedTitle + '&body=' + encodedUrl
+          );
+
+          togglePanel(_panel);
+        }
+      }
+    }();
+
+    var setupHint = function () {
+      _panel = document.getElementById('setup-hint');
+      var settingsLink = document.getElementById('open-settings');
+      settingsLink.onclick = function settingsLinkClickHandler(e) {
+        e.preventDefault();
+        browser.runtime.openOptionsPage();
+        window.close();
+      };
+      return {
+        show: function () {
+          togglePanel(_panel);
+        }
+      }
+    }();
 
     var activePanel = document.getElementById('loading-panel');
     togglePanel(activePanel);
 
-    var showErrorPanel = function (message) {
-      message = message || 'Something went wrong :('
-      togglePanel(errorPanel, message);
-    };
-
-    var showPreviewPanel = function () {
-      togglePanel(previewPanel);
-    };
-
-    var showSetupHint = function () {
-      togglePanel(setupHint);
-    };
-
-    function togglePanel(panel, message) {
+    function togglePanel(panel) {
       if (activePanel != panel || activePanel.style.display == 'none') {
         activePanel.style.display = 'none';
         panel.style.display = 'inherit';
         activePanel = panel;
-
-        if (panel == errorPanel) {
-          errorMessage.innerText = message;
-        }
       }
     }
 
-    settingsLink.onclick = function settingsLinkClickHandler(e) {
-      e.preventDefault();
-      browser.runtime.openOptionsPage();
-      window.close();
-    };
-
     return {
-      showError: showErrorPanel,
-      showPreview: showPreviewPanel,
-      showSetupHint: showSetupHint
+      showError: errorPanel.show,
+      showPreview: previewPanel.show,
+      showSetupHint: setupHint.show
     }
   }();
 
@@ -67,12 +87,6 @@
         panels.showSetupHint();
         return;
       }
-      var url = null;
-      var title = null;
-
-      var titleElement = document.getElementById('title');
-      var urlElement = document.getElementById('url');
-      var mailLink = document.getElementById('mail-link');
 
       var gettingActiveTab = browser.tabs.query({
         active: true,
@@ -81,19 +95,11 @@
       gettingActiveTab.then((tabs) => {
         if (tabs[0]) {
           // done loading
-          panels.showPreview();
 
-          title = tabs[0].title;
-          url = tabs[0].url;
+          let title = tabs[0].title;
+          let url = tabs[0].url;
 
-          titleElement.innerText = title;
-          urlElement.innerText = url;
-
-          var encodedTitle = encodeURI(title);
-          var encodedUrl = encodeURI(url);
-          mailLink.href = (
-            'mailto:' + targetAddress + '?subject=' + encodedTitle + '&body=' + encodedUrl
-          );
+          panels.showPreview(targetAddress, title, url);
         }
       });
 
