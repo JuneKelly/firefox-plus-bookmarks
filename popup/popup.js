71bdadc977990e5057b28ebbb9830efaa7156654
halbryard <35330227+halbryard@users.noreply.github.com>
2018-01-16 11:12:41 +0100

    panels objects encapsulation and DRY panel's toggle code
    - encapsulation of panel objects;
    - togglePanel function for mutual exclusive (repeated) panels display;
    - minor published methods and client code renames.

--- popup/popup.js
+++ popup/popup.js
@@ -1,50 +1,70 @@
 // popup js
 
 (function () {
-  window.onerror = function(e) {
-    document.querySelector('body').innerText = 'ERROR! '+e;
+  window.onerror = function (e) {
+    document.querySelector('body').innerText = 'ERROR! ' + e;
   };
 
-  var errorPanel = document.getElementById('error-panel');
-  var errorMessage = document.getElementById('error-message');
-  var loadingPanel = document.getElementById('loading-panel');
-  var previewPanel = document.getElementById('preview-panel');
-  var setupHint = document.getElementById('setup-hint');
-  var settingsLink = document.getElementById('open-settings');
+  var panels = function () {
 
-  var showErrorPanel = function(message) {
-    errorMessage.innerText = message || 'Something went wrong :(';
-    errorPanel.style.display = 'inherit';
-    loadingPanel.style.display = 'none';
-    setupHint.style.display = 'none';
-    previewPanel.style.display = 'none';
-  };
+    var errorPanel = document.getElementById('error-panel');
+    var errorMessage = document.getElementById('error-message');
+    /* 
+      UNUSED panel object loadingPanel:
+      removed but still used as default starting panel
+      (see activePanel initialization);
+    // var loadingPanel = document.getElementById('loading-panel');
+    */
+    var previewPanel = document.getElementById('preview-panel');
+    var setupHint = document.getElementById('setup-hint');
+    var settingsLink = document.getElementById('open-settings');
 
-  var showPreviewPanel = function() {
-    errorPanel.style.display = 'none';
-    loadingPanel.style.display = 'none';
-    setupHint.style.display = 'none';
-    previewPanel.style.display = 'inherit';
-  };
+    var activePanel = document.getElementById('loading-panel');
+    togglePanel(activePanel);
 
-  var showSetupHint = function() {
-    errorPanel.style.display = 'none';
-    loadingPanel.style.display = 'none';
-    setupHint.style.display = 'inherit';
-    previewPanel.style.display = 'none';
-  };
+    var showErrorPanel = function (message) {
+      message = message || 'Something went wrong :('
+      togglePanel(errorPanel, message);
+    };
 
-  settingsLink.onclick = function settingsLinkClickHandler(e) {
-    e.preventDefault();
-    browser.runtime.openOptionsPage();
-    window.close();
-  };
+    var showPreviewPanel = function () {
+      togglePanel(previewPanel);
+    };
+
+    var showSetupHint = function () {
+      togglePanel(setupHint);
+    };
+
+    function togglePanel(panel, message) {
+      if (activePanel != panel || activePanel.style.display == 'none') {
+        activePanel.style.display = 'none';
+        panel.style.display = 'inherit';
+        activePanel = panel;
+
+        if (panel == errorPanel) {
+          errorMessage.innerText = message;
+        }
+      }
+    }
+
+    settingsLink.onclick = function settingsLinkClickHandler(e) {
+      e.preventDefault();
+      browser.runtime.openOptionsPage();
+      window.close();
+    };
+
+    return {
+      showError: showErrorPanel,
+      showPreview: showPreviewPanel,
+      showSetupHint: showSetupHint
+    }
+  }();
 
   browser.storage.local.get('email').then(
     function resultHandler(result) {
       var targetAddress = result.email;
       if (!targetAddress) {
-        showSetupHint();
+        panels.showSetupHint();
         return;
       }
       var url = null;
@@ -54,11 +74,14 @@
       var urlElement = document.getElementById('url');
       var mailLink = document.getElementById('mail-link');
 
-      var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+      var gettingActiveTab = browser.tabs.query({
+        active: true,
+        currentWindow: true
+      });
       gettingActiveTab.then((tabs) => {
         if (tabs[0]) {
           // done loading
-          showPreviewPanel();
+          panels.showPreview();
 
           title = tabs[0].title;
           url = tabs[0].url;
@@ -69,14 +92,14 @@
           var encodedTitle = encodeURI(title);
           var encodedUrl = encodeURI(url);
           mailLink.href = (
-            'mailto:'+targetAddress+'?subject='+encodedTitle+'&body='+encodedUrl
+            'mailto:' + targetAddress + '?subject=' + encodedTitle + '&body=' + encodedUrl
           );
         }
       });
 
     },
     function errorHandler(e) {
-      showErrorPanel(e.message);
+      panels.showError(e.message);
     }
   );
 
